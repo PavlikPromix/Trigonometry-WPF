092674dc7eea10bdba32343c529fb75d6c0f80df
PavlikPromix <beriya2004@mail.ru>
2021-03-28 21:28:35 +0300

    Fixed SliderSync; Added visual demonstration;

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,37 +14,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-namespace Trigonometry_WPF
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace Trigonometry_WPF 
+{ 
     public partial class MainWindow : Window
     {
-        double xval;
+        #region Properties
+        public double Alpha;
+        private double x;
+        public double X { get { return x; } set { x = value; } }
+        private double y;
+        public double Y { get { return y; } set { y = value; } }
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void sinslider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            xval = RadsToDeegs(Math.Asin(sinslider.Value));
-            cosslider.Value = Math.Cos(xval);
-            anglable.Content = $"Angle: {xval}";
+            if (sender == sinslider)
+            {
+                if (cosslider.Value != Math.Sqrt(1 - Math.Pow(sinslider.Value, 2)))
+                {
+                    cosslider.Value = Math.Sqrt(1 - Math.Pow(sinslider.Value, 2));
+                }
+            }
+            else
+            {
+                if (sinslider.Value != Math.Sqrt(1 - Math.Pow(cosslider.Value, 2)))
+                {
+                    sinslider.Value = Math.Sqrt(1 - Math.Pow(cosslider.Value, 2));
+                }
+            }
+            Alpha = Math.Acos(cosslider.Value);
+            coslable.Content = $"Cos: {Math.Round(cosslider.Value, 2)}";
+            sinlabel.Content = $"Sin: {Math.Round(sinslider.Value, 2)}";
+            anglable.Content = $"Angle: {Math.Round(RadToDeg(Alpha), 2)}";
+            Draw();
         }
-
-        private void cosslider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            xval = RadsToDeegs(Math.Acos(sinslider.Value));
-            sinslider.Value = Math.Sin(xval);
-            anglable.Content = $"Angle: {xval}";
-        }
-        public static double RadsToDeegs(double radians)
+        public static double RadToDeg(double radians)
         {
             double degrees = (180 / Math.PI) * radians;
             return (degrees);
+        }
+        public void Draw()
+        {
+            demoLine.X2 = X;
+            demoLine.Y2 = Y;
+            Update();
+        }
+        public void Update()
+        {
+            X = 200 + 100 * Math.Cos(Alpha);
+            Y = 200 + 100 * Math.Sin(Alpha);
         }
     }
 }
